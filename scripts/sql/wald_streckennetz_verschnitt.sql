cca9637de098e726c15497293a51e003384c1b01
Volker Eichhorn <mail@volker.io>
2017-10-20 21:09:11 +0200

    Query: wald_streckennetz_verschnitt

---
+++ scripts/sql/wald_streckennetz_verschnitt.sql
@@ -0,0 +1,54 @@
+CREATE TABLE its_hackathon.wald_streckennetz_verschnitt AS
+WITH wald_polys AS (
+  SELECT
+    cl.*
+  FROM
+    its_hackathon.corine_landcover_10 cl
+  WHERE
+    cl.clc in ('311', '312','313')
+),
+gleise_within AS (
+  SELECT
+    db.*
+  FROM
+    wald_polys  c,
+    its_hackathon.db_streckennetz db
+  WHERE
+    ST_Within(db.geom, c.geom)
+),
+gleise_intersects AS (
+  SELECT
+    db.*
+  FROM
+    wald_polys  c,
+    its_hackathon.db_streckennetz db
+  WHERE
+    ST_Intersects(c.geom, db.geom)
+),
+intersection AS (
+  SELECT
+    db.id,
+    ST_Intersection(db.geom, wp.geom) AS geom,
+    db.mifcode,
+    db.strecke_nr,
+    db.richtung,
+    db.laenge,
+    db.von_km_i,
+    db.bis_km_i,
+    db.von_km_l,
+    db.bis_km_l,
+    db.elektrifiz,
+    db.bahnnutzun,
+    db.geschwindi,
+    db.strecke_ku,
+    db.gleisanzah,
+    db.bahnart,
+    db.kmspru_typ,
+    db.kmspru_t00
+  FROM
+    gleise_intersects db,
+    wald_polys wp
+)
+SELECT * FROM gleise_within g
+UNION
+SELECT * FROM intersection;
